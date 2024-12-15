7e5cef6f20db2eed6282b0f4db940ce4b30ec923
pchchv <ipchchv@gmail.com>
2024-12-15 16:25:32 +0300

    Create a LogLevel type

--- src/options.rs
+++ src/options.rs
@@ -1,3 +1,16 @@
 //! This module contains the configuration of the application.
 //!
 //! All options are passed individually to each function and are not bundled together.
+
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LogLevel {
+    /// Write everything
+    All,
+    /// No serving messages
+    NoServeStatus,
+    /// No startup messages, but yes auth data
+    NoStartup,
+    /// No auth data
+    NoAuth,
+}
