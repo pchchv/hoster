3d4b63228d4e1547c46df93da1ab6a703090b123
pchchv <ipchchv@gmail.com>
2024-12-15 16:45:18 +0300

    Create a type CacheT

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,5 +1,13 @@
+use blake3;
+use std::collections::HashMap;
+use std::sync::atomic::AtomicU64;
+use iron::headers::Encoding;
+
+
 mod webdav;
 
+
+type CacheT<Cnt> = HashMap<(blake3::Hash, Encoding), (Cnt, AtomicU64)>;
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum WebDavLevel {
